809a2667aa49a679e8589916e78b0915cdbf8063
FrigoAlex <alejo012903@hotmail.com>
2023-03-27 03:08:38 -0500

    ✨ Add useHistory hook

---
+++ src/hooks/useHistory.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect, useRef } from "react";
+
+function useHistory<Type>(state: Type) {
+  const [history, setHistory] = useState<Type[]>([]);
+  const refPreviousValue = useRef<Type>();
+  const PreviousValue = refPreviousValue.current;
+
+  useEffect(() => {
+    refPreviousValue.current = history.at(-1);
+    if (state && state !== history.at(-1)) {
+      setHistory([...history, state]);
+    }
+  }, [state]);
+
+  const removeItem = (item: Type) => {
+    const newHistory = [...history];
+    const index = newHistory.findIndex((i) => i === item);
+    if (index !== -1) {
+      newHistory.splice(index, 1);
+      setHistory(newHistory);
+    }
+  };
+  const getPreviousValue = (n: number) => {
+    return history.at(n);
+  };
+
+  const clear = () => {
+    setHistory([]);
+  };
+  const length = history.length;
+  return { getPreviousValue, PreviousValue, removeItem, length, history, clear};
+}
+
+export default useHistory;
