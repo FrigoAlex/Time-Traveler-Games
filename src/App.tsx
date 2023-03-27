83081911cde392b9c79b147f2913db0158271340
FrigoAlex <alejo012903@hotmail.com>
2023-03-27 03:45:44 -0500

    ✨ Add App component with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import NoContent from "./pages/NoContent/NoContent";
+import TicTacToe from "./pages/TicTacToe/TicTacToe";
+import TimeMachine from "./pages/TimeMachine/TimeMachine";
+import { PageRoutes } from "./types/pageRoutes";
+
 import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>Time Traveler Games</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route
+          path={PageRoutes.HOME}
+          element={<Home />}
+          errorElement={<NoContent redirect />}
+        />
+        <Route path={PageRoutes.TIMEMACHINE} element={<TimeMachine />} />
+        <Route path={PageRoutes.TICTACTOE} element={<TicTacToe />} />
+        <Route path={PageRoutes.NO_CONTENT} element={<NoContent redirect />} />
+        <Route path="*" element={<NoContent redirect />} />
+      </Routes>
+    </Router>
   );
 };
 
