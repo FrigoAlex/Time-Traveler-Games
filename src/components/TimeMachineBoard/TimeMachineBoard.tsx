5b9489646749507d44e09c40b3a7ad357feec8f8
FrigoAlex <alejo012903@hotmail.com>
2023-03-27 03:11:50 -0500

    ✨ Create timeMachineBoard component

---
+++ src/components/TimeMachineBoard/TimeMachineBoard.tsx
@@ -0,0 +1,72 @@
+interface TimeMachineProps {
+  prevFoo?: () => void;
+  nextFoo?: () => void;
+  resumeFoo?: () => void;
+  prevItem?: number;
+  item?: number;
+  setnewItem?: (x: number) => void;
+}
+
+export default function TimeMachine({
+  prevFoo,
+  nextFoo,
+  resumeFoo,
+  prevItem,
+  item,
+  setnewItem,
+}: TimeMachineProps) {
+  return (
+    <>
+      <div className="grid-container">
+        <div className="grid4x4">
+          {Array.from({ length: 16 }, (_, x) => (
+            <div
+              className={`grid-elements${
+                item === x + 1
+                  ? " selected"
+                  : prevItem === x + 1
+                    ? " prevSelected"
+                    : ""
+              }`}
+              key={x + 1}
+              onClick={() => {
+                setnewItem && setnewItem(x + 1);
+              }}
+            >
+              {x + 1}
+            </div>
+          ))}
+        </div>
+        <div className="menu">
+          <button
+            className="game-button"
+            disabled={!prevFoo}
+            onClick={() => {
+              prevFoo && prevFoo();
+            }}
+          >
+            Previous
+          </button>
+          <button
+            className="game-button"
+            disabled={!resumeFoo}
+            onClick={() => {
+              resumeFoo && resumeFoo();
+            }}
+          >
+            Resume
+          </button>
+          <button
+            className="game-button"
+            disabled={!nextFoo}
+            onClick={() => {
+              nextFoo && nextFoo();
+            }}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
