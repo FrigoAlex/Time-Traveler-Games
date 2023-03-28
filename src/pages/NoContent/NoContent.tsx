29e5c41d3ceec00161bf5684a754b8fd5044b727
FrigoAlex <alejo012903@hotmail.com>
2023-03-27 22:06:09 -0500

    ⚰️ Remove useEffect and redirect code

--- src/pages/NoContent/NoContent.tsx
+++ src/pages/NoContent/NoContent.tsx
@@ -1,22 +1,12 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
 import SadFace from "../../assets/svg/sad-face.svg";
-import { PageRoutes } from "../../types/pageRoutes";
 
 import "./NoContent.css";
 
-const NoContent = ({ redirect }) => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (redirect) navigate(PageRoutes.NO_CONTENT);
-  }, [redirect]);
-
+const NoContent = () => {
   return (
     <div className="home-page">
       <h2 className="no-content-title">404 - Page not found</h2>
-      <div className="no-content-container" >
+      <div className="no-content-container">
         <img src={SadFace} alt="sad face" className="sad-face" />
       </div>
     </div>
